f03cb4e0fe155234e8bc15d090ef0f28a00d700c
ssvprasad144 <ssvprasad144@gmail.com>
2025-12-21 12:38:27 +0530

    Update settings.py

--- careerinntech/careerinntech/settings.py
+++ careerinntech/careerinntech/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # ================= BASE =================
 
@@ -86,11 +88,13 @@ TEMPLATES = [
 # SQLite (local + initial Render deploy)
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 # ================= PASSWORD VALIDATION =================
 
