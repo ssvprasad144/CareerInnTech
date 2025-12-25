a55b55459e04748d692b23a386618702b0b24ac9
ssvprasad144 <ssvprasad144@gmail.com>
2025-12-25 20:14:24 +0530

    Update urls.py

--- careerinntech/college/urls.py
+++ careerinntech/college/urls.py
@@ -4,4 +4,6 @@ from .views import ts_eamcet_colleges, ap_eamcet_colleges
 urlpatterns = [
     path("ts-eamcet/", ts_eamcet_colleges, name="ts_eamcet"),
     path("ap-eamcet/", ap_eamcet_colleges, name="ap_eamcet"),
+    path("ai/chat/", views.ai_chat, name="ai_chat"),
+
 ]
