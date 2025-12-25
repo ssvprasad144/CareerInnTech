98b77dd10a23b2567daffc1c4a265c16c5a9b0e2
ssvprasad144 <ssvprasad144@gmail.com>
2025-12-25 21:36:46 +0530

    Update urls.py

--- careerinntech/college/urls.py
+++ careerinntech/college/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from . import views 
-
-
+from . import views
 
 urlpatterns = [
-    path("ts-eamcet/", ts_eamcet_colleges, name="ts_eamcet"),
-    path("ap-eamcet/", ap_eamcet_colleges, name="ap_eamcet"),
+    path("ts-eamcet/", views.ts_eamcet_colleges, name="ts_eamcet"),
+    path("ap-eamcet/", views.ap_eamcet_colleges, name="ap_eamcet"),
     path("ai/chat/", views.ai_chat, name="ai_chat"),
-
 ]
