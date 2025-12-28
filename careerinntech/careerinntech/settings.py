0c5ae37c3526c0e0478a8d591d9519e42b186123
ssvprasad144 <ssvprasad144@gmail.com>
2025-12-28 07:03:09 +0530

    Local changes to settings and requirements

--- careerinntech/careerinntech/settings.py
+++ careerinntech/careerinntech/settings.py
@@ -32,7 +32,7 @@ else:
 
 # ================= DEBUG =================
 
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # ================= ALLOWED HOSTS =================
 
