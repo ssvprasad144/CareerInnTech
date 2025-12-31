ca64d72202d9355f608b155665aa2b4e469ebf6b
ssvprasad144 <ssvprasad144@gmail.com>
2025-12-31 18:48:32 +0530

    Update views.py

--- careerinntech/projects/views.py
+++ careerinntech/projects/views.py
@@ -3,16 +3,21 @@ from .data import PERSONALIZED_PROJECTS
 
 def projects_home(request):
 
+    # Read filters (safe defaults)
     track = request.GET.get("track", "btech-cse")
     project_type = request.GET.get("type", "personalized")
 
-    data = PERSONALIZED_PROJECTS.get(track, {})
-    projects = data.get(project_type, {})
+    # Get track data (must be dict)
+    track_data = PERSONALIZED_PROJECTS.get(track, {})
+
+    # If structure is correct, this is a dict
+    # NOT a list
+    projects = track_data if isinstance(track_data, dict) else {}
 
     context = {
-        "track": track,
-        "project_type": project_type,
         "projects": projects,
+        "selected_track": track,
+        "selected_type": project_type,
     }
 
     return render(request, "projects/projects.html", context)
