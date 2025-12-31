ce7d62d69929eff50e65db98f15b6fe035464a13
ssvprasad144 <ssvprasad144@gmail.com>
2025-12-31 10:45:48 +0530

    Delete careerinntech/placements/views.py

--- careerinntech/placements/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .data import PLACEMENT_MODULES
-
-def placement_preparation(request):
-    return render(
-        request,
-        "placements/placement_preparation.html",
-        {"modules": PLACEMENT_MODULES},
-    )
