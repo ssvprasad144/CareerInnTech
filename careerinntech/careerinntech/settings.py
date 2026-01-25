59d6af4e57634433842f05803c706afbbd0dff8c
maniteja2230 <23211a6674@bvrit.ac.in>
2026-01-26 00:46:25 +0530

    Update settings.py

--- careerinntech/careerinntech/settings.py
+++ careerinntech/careerinntech/settings.py
@@ -226,6 +226,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'yourgmail@gmail.com'
-EMAIL_HOST_PASSWORD = 'APP_PASSWORD'
+EMAIL_HOST_USER = 'careerinntech@gmail.com'
+EMAIL_HOST_PASSWORD = 'vgtt ouhp qvtk mhrm'
 
