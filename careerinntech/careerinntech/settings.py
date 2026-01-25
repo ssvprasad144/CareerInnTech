fec94ac2de49daf79d83861af89fa941bfe5790c
maniteja2230 <23211a6674@bvrit.ac.in>
2026-01-25 23:59:53 +0530

    Update settings.py

--- careerinntech/careerinntech/settings.py
+++ careerinntech/careerinntech/settings.py
@@ -221,3 +221,11 @@ MEDIA_ROOT = BASE_DIR / "media"
 # ================= DEFAULT FIELD =================
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'yourgmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'APP_PASSWORD'
+
