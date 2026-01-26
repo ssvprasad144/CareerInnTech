374638226186a609a841642d3f5a2a07d9b0d1ec
maniteja2230 <23211a6674@bvrit.ac.in>
2026-01-26 22:07:14 +0530

    Update models.py

--- careerinntech/core/models.py
+++ careerinntech/core/models.py
@@ -150,7 +150,6 @@ class UserContextMemory(models.Model):
 
 
 class SignupOTP(models.Model):
-    phone = models.CharField(max_length=20, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     otp = models.CharField(max_length=6)
     created_at = models.DateTimeField(auto_now_add=True)
